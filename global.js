46f0ebbdf246a5c2b2a7f99127a71a5852513bf5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-10-24 12:50:32 +0200

    Don't leak other exports from `global.js`

--- global.js
+++ global.js
@@ -1,1 +1,8 @@
-global.structuredClone = require('.')
+const { serializeWithTransfer, deserializeWithTransfer } = require('.')
+
+global.structuredClone = function structuredClone(value, opts = {}) {
+  return deserializeWithTransfer(
+    serializeWithTransfer(value, opts.transfer, opts.interfaces),
+    opts.interfaces
+  )
+}
